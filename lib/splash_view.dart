908510f318671b825c2d7694d02bfa9a1884a942
AshrafElManDoh <ashraftrezegeh@gmail.com>
2025-11-06 02:00:55 +0200

    add splash view ui

--- lib/splash_view.dart
+++ lib/splash_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hungry_app/core/constants/app_colors.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -6,7 +8,16 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      backgroundColor: AppColors.primaryColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 250,),
+          SvgPicture.asset("assets/logo/hungry.svg"),
+          Spacer(),
+          Image.asset("assets/splash/splash.png"),
+        ],
+      ),
     );
   }
 }
